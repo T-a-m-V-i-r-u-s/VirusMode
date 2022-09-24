c98b6e9bd20a0d0e2b4cd095d0cdca3300bab325
grant <grantvanbreda@gmail.com>
2022-09-24 14:22:17 +1000

    broken rick roll

--- popup.js
+++ popup.js
@@ -22,3 +22,31 @@ function setPageBackgroundColor() {
     document.body.style.backgroundColor = color;
   });
 }
+
+
+/* TOGGLE SETTINGS */
+// Get toggle elements
+let rickMode = document.getElementById("rickRoll");
+
+// Update the toggle elements when the page loads
+chrome.storage.sync.get("rickRoll", ({ rickRoll }) => {
+  rickMode.checked = rickRoll;
+});
+
+// Function to toggle the rick roll variable
+function toggleRickRoll() {
+  chrome.storage.sync.get("rickRoll", ({ rickRoll }) => {
+    chrome.storage.sync.set({ rickRoll: !rickRoll });
+    console.log('Rick roll set to %c' + !rickRoll, `rickRoll: ${rickRoll}`);
+  });
+}
+
+// When the button is clicked, toggle the rick roll variable
+rickMode.addEventListener("click", async () => {
+  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    func: toggleRickRoll,
+  });
+});
